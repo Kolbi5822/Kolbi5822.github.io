582c7805a019e3668b20ca5845b84fddad86e3bc
Kolbi Lee <kolbi5822@gmail.com>
2025-09-08 22:08:28 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kolbi5822.github.io"
+  base: "/kolbi-portfolio"
 })
